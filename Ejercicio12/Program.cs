09893fccbc3bc0ccd26b5095f712cac83d0d3404
OSCAR ALBERTO MORA M�NDEZ <oscarmora18268@correo.itm.edu.co>
2023-09-15 21:09:26 -0500

    terminar ejer

---
+++ Ejercicio12/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace ejercicio12
+{
+    public class program
+    {
+        static void Main(string[] Args)
+        {
+            // declaracion de variables
+
+            int num1, num2, min, max;
+            const string mensaje = "Por favor ingresar un numero entero valido";
+            bool retorno = false;
+
+            // pedir datos
+
+            do
+            {
+                Console.WriteLine("Ingrese el primer numero: ");
+                if (!int.TryParse(Console.ReadLine(), out num1))
+                {
+                    Console.WriteLine(mensaje);
+                    retorno = true;
+                }
+            }
+            while (retorno);
+
+            do
+            {
+                Console.WriteLine("Ingrese el segundo numero: ");
+                if (!int.TryParse(Console.ReadLine(), out num2))
+                {
+                    Console.WriteLine(mensaje);
+                    retorno = true;
+                }
+            }
+            while (retorno);
+
+            //calculos
+
+            min = Math.Min(num1, num2);
+            max = Math.Max(num1, num2);
+
+
+
+        }
+    }
+}
