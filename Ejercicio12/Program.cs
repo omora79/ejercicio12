1e11fd2136e702c464cf3310b93db6c3b2b60d30
OSCAR ALBERTO MORA M�NDEZ <oscarmora18268@correo.itm.edu.co>
2023-09-22 14:50:19 -0500

    ter

--- Ejercicio12/Program.cs
+++ Ejercicio12/Program.cs
@@ -13,43 +13,62 @@
             int num1, num2, min, max, salida;
             const string mensaje = "Por favor ingresar un numero entero valido";
             bool retorno = false;
-
+            bool reinicio = true;
+            string respuesta;
             // pedir datos
-
-            do
+            
+            while (reinicio)
             {
-                Console.WriteLine("Ingrese el primer numero: ");
-                if (!int.TryParse(Console.ReadLine(), out num1))
+                do
                 {
-                    Console.WriteLine(mensaje);
-                    retorno = true;
+                    Console.WriteLine("Ingrese el primer numero: ");
+                    if (!int.TryParse(Console.ReadLine(), out num1))
+                    {
+                        Console.WriteLine(mensaje);
+                        retorno = true;
+                    }
                 }
-            }
-            while (retorno);
+                while (retorno);
 
-            do
-            {
-                Console.WriteLine("Ingrese el segundo numero: ");
-                if (!int.TryParse(Console.ReadLine(), out num2))
+                do
                 {
-                    Console.WriteLine(mensaje);
-                    retorno = true;
+                    Console.WriteLine("Ingrese el segundo numero: ");
+                    if (!int.TryParse(Console.ReadLine(), out num2))
+                    {
+                        Console.WriteLine(mensaje);
+                        retorno = true;
+                    }
                 }
+                while (retorno);
+
+                //calculos
+
+                min = Math.Min(num1, num2);
+                max = Math.Max(num1, num2);
+
+                salida = min;
+
+                while (salida <= max)
+                {
+                    Console.Write(string.Format("{0} ", salida));
+                    salida++;
+                }
+
+                Console.WriteLine("desea realizar otro nivel (s/n):  ");
+
+                respuesta = Console.ReadLine();
+
+                if (respuesta != "s")
+                {
+                    reinicio = false;
+                }
+                                
             }
-            while (retorno);
 
-            //calculos
 
-            min = Math.Min(num1, num2);
-            max = Math.Max(num1, num2);
 
-            salida = min;
 
-            while (salida <= max)
-            {
-                Console.Write(string.Format("{0} ", salida));
-                salida++;
-            }
+            
 
         }
     }
